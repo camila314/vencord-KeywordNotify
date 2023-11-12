19b9b84bd064304d41a682be6f6a4e8de08199e6
camila <47485054+camila314@users.noreply.github.com>
2023-11-11 22:37:39 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,20 +1,8 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 exhq
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Vencord, a Discord client mod
+ * Copyright (c) 2023 Vendicated, camila314, and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 import definePlugin, { OptionType } from "@utils/types";
 import { DataStore } from "@api/index";
@@ -22,11 +10,12 @@ import { definePluginSettings } from "@api/Settings";
 import { DeleteIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
 import { Flex } from "@components/Flex";
-import { TextInput, useState, Forms, Button } from "@webpack/common";
+import { TextInput, useState, Forms, Button, UserStore, UserUtils } from "@webpack/common";
 import { useForceUpdater } from "@utils/react";
 import "./style.css";
 
 let regexes = [];
+let me = null;
 
 async function setRegexes(idx: number, reg: string) {
     regexes[idx] = reg;
@@ -102,19 +91,35 @@ export default definePlugin({
     authors: [Devs.camila314],
     description: "Sends a notification if a given message matches certain keywords or regexes",
     settings,
-    patches: [{
-	    find: "isRawMessageMentioned:",
-	    replacement: {
-            match: /isRawMessageMentioned:function\(\){return (.{1,2}).{1,512}function \1\(.{1,512}?=(.{1,2});return/,
-            replace: "$& Vencord.Plugins.plugins.KeywordNotify.contains($2) ||"
-	    }
-	}],
+    patches: [
+        {
+    	    find: "}_dispatch(",
+    	    replacement: {
+                match: /}_dispatch\((.{1,2}),.{1,2}\){/,
+                replace: "$&$1=$self.modify($1);"
+    	    }
+    	}
+    ],
 
     async start() {
         regexes = await DataStore.get("KeywordNotify_rules") ?? [];
+        me = await UserUtils.getUser(UserStore.getCurrentUser().id);
     },
 
-    contains(e) {
-        return regexes.some(a => a != "" && e.rawMessage.content.match(new RegExp(a)));
+    applyRegexes(m) {
+        if (regexes.some(r => m.content.match(new RegExp(r)))) {
+            m.mentions.push(me);
+        }
+    },
+
+    modify(e) {
+        if (e.type == "MESSAGE_CREATE") {
+            this.applyRegexes(e.message);
+        } else if (e.type == "LOAD_MESSAGES_SUCCESS") {
+            for (let msg = 0; msg < e.messages.length; ++msg) {
+                this.applyRegexes(e.messages[msg]);
+            }
+        }
+        return e;
     }
 });
