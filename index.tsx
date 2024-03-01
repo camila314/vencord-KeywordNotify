22df5664ccd3c661181b8184bf56a19c4b6ee8d0
x3rt <x3rtdev@gmail.com>
2024-02-29 18:24:46 -0700

    Add per-keyword whitelist/blacklist

--- index.tsx
+++ index.tsx
@@ -9,37 +9,38 @@ import { DataStore } from "@api/index";
 import { definePluginSettings } from "@api/Settings";
 import { DeleteIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
+import { Margins } from "@utils/margins";
 import { Flex } from "@components/Flex";
-import { TextInput, useState, Forms, Button, UserStore, UserUtils, TabBar, ChannelStore, SelectedChannelStore  } from "@webpack/common";
+import { TextInput, useState, Forms, Button, UserStore, UserUtils, TabBar, ChannelStore, SelectedChannelStore, SearchableSelect } from "@webpack/common";
 import { useForceUpdater } from "@utils/react";
 import { findByCodeLazy, findByPropsLazy } from "@webpack";
-import { Message } from "discord-types/general/index.js";
+import { Message, User} from "discord-types/general/index.js";
 import "./style.css";
 
-let regexes: Array<string> = [];
-let listIds: Array<string> = [];
+let keywordEntries: Array<{ regex: string, listIds: Array<string>, listType: ListType }> = [];
+let currentUser: User;
+let keywordLog: Array<any> = [];
 
 const MenuHeader = findByCodeLazy("useInDesktopNotificationCenterExperiment)(");
 const Popout = findByPropsLazy("ItemsPopout");
 const recentMentionsPopoutClass = findByPropsLazy("recentMentionsPopout");
 
-const { createMessageRecord } = findByPropsLazy("createMessageRecord", "updateMessageRecord");
+const {createMessageRecord} = findByPropsLazy("createMessageRecord", "updateMessageRecord");
 
-
-async function setRegexes(idx: number, reg: string) {
-    regexes[idx] = reg;
-    await DataStore.set("KeywordNotify_rules", regexes);
-}
-
-async function removeRegex(idx: number, updater: () => void) {
-    regexes.splice(idx, 1);
-    await DataStore.set("KeywordNotify_rules", regexes);
+async function addKeywordEntry(updater: () => void) {
+    keywordEntries.push({regex: "", listIds: [], listType: ListType.BlackList});
+    await DataStore.set("KeywordNotify_keywordEntries", keywordEntries);
     updater();
 }
 
-async function addRegex(updater: () => void) {
-    regexes.push("");
-    await DataStore.set("KeywordNotify_rules", regexes);
+async function setKeywordEntry(idx: number, reg: string, listIds: Array<string>, listType: ListType) {
+    keywordEntries[idx] = {regex: reg, listIds, listType};
+    await DataStore.set("KeywordNotify_keywordEntries", keywordEntries);
+}
+
+async function removeKeywordEntry(idx: number, updater: () => void) {
+    keywordEntries.splice(idx, 1);
+    await DataStore.set("KeywordNotify_keywordEntries", keywordEntries);
     updater();
 }
 
@@ -51,80 +52,21 @@ function safeMatchesRegex(s: string, r: string) {
     }
 }
 
-async function setList(idx: number, reg: string) {
-    listIds[idx] = reg;
-    await DataStore.set("KeywordNotify_listIds", listIds);
-}
-
-async function removeList(idx: number, updater: () => void) {
-    listIds.splice(idx, 1);
-    await DataStore.set("KeywordNotify_listIds", listIds);
-    updater();
-}
-
-async function addList(updater: () => void) {
-    listIds.push("");
-    await DataStore.set("KeywordNotify_listIds", listIds);
-    updater();
-}
 
 enum ListType {
     BlackList = "BlackList",
     Whitelist = "Whitelist"
 }
 
-function ListedIds() {
-    const update = useForceUpdater();
-    const [values, setValues] = useState(listIds);
-
-    const elements = listIds.map((a, i) => {
-        const setValue = (v: string) => {
-            let valuesCopy = [...values];
-            valuesCopy[i] = v;
-            setValues(valuesCopy);
-        };
-
-        return (
-            <Flex flexDirection="row">
-                <div style={{ flexGrow: 1 }}>
-                    <TextInput
-                        placeholder="ID"
-                        spellCheck={false}
-                        value={values[i]}
-                        onChange={setValue}
-                        onBlur={() => setList(i, values[i])}
-                    />
-                </div>
-                <Button
-                    onClick={() => removeList(i, update)}
-                    look={Button.Looks.BLANK}
-                    size={Button.Sizes.ICON}
-                    className="keywordnotify-delete">
-                    <DeleteIcon/>
-                </Button>
-            </Flex>
-        );
-    });
-
-    return (
-        <>
-            <Forms.FormTitle tag="h4">White/Blacklisted Ids</Forms.FormTitle>
-            <Forms.FormText tag="div">Supports user, channel, and guild ids</Forms.FormText>
-            {elements}
-            <div><Button onClick={() => addList(update)}>Add ID</Button></div>
-        </>
-    );
-}
-
 function highlightKeywords(s: string, r: Array<string>) {
-    let reg;
+    let regex: RegExp;
     try {
-        reg = new RegExp(r.join("|"), "g");
+        regex = new RegExp(r.join("|"), "g");
     } catch {
         return [s];
     }
 
-    let matches = s.match(reg);
+    let matches = s.match(regex);
     if (!matches)
         return [s];
 
@@ -141,18 +83,159 @@ function highlightKeywords(s: string, r: Array<string>) {
     ]);
 }
 
-function listContains(id: string) {
-    return listIds.some((e) => e === id);
+function Collapsible({title, children}) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+            <Button
+                onClick={() => setIsOpen(!isOpen)}
+                look={Button.Looks.BLANK}
+                size={Button.Sizes.ICON}
+                className="keywordnotify-collapsible">
+                <div style={{display: "flex", alignItems: "center"}}>
+                    <div style={{marginLeft: "auto", color: "var(--text-muted)"}}>{isOpen ? "▲" : "▼"}</div>
+                    <Forms.FormTitle tag="h4">{title}</Forms.FormTitle>
+                </div>
+            </Button>
+            {isOpen && children}
+        </div>
+    );
 }
 
-function listedMessage(m: Message) {
-    if (listContains(m.channel_id) || listContains(m.author.id)) {
-        return true;
-    }
-    const channel = ChannelStore.getChannel(m.channel_id);
-    return channel != null && listContains(channel.guild_id);
+function ListedIds({listIds, setListIds}) {
+    const update = useForceUpdater();
+    const [values, setValues] = useState(listIds);
+
+    const elements = values.map((id, i) => {
+        const setId = (v: string) => {
+            let valuesCopy = [...values];
+            valuesCopy[i] = v;
+            setValues(valuesCopy);
+        };
+
+        return (
+            <Flex flexDirection="row" style={{marginBottom: "5px"}}>
+                <div style={{flexGrow: 1}}>
+                    <TextInput
+                        placeholder="ID"
+                        spellCheck={false}
+                        value={values[i]}
+                        onChange={setId}
+                        onBlur={() => setListIds(values)}
+                    />
+                </div>
+                <Button
+                    onClick={() => {
+                        values.splice(i, 1);
+                        setListIds(values);
+                        update();
+                    }}
+                    look={Button.Looks.BLANK}
+                    size={Button.Sizes.ICON}
+                    className="keywordnotify-delete">
+                    <DeleteIcon/>
+                </Button>
+            </Flex>
+        );
+    });
+
+    return (
+        <>
+            {elements}
+        </>
+    );
+}
+
+function ListTypeSelector({listType, setListType}) {
+    return (
+        <SearchableSelect
+            options={[
+                {label: "Whitelist", value: ListType.Whitelist},
+                {label: "Blacklist", value: ListType.BlackList}
+            ]}
+            placeholder={"Select a list type"}
+            maxVisibleItems={2}
+            closeOnSelect={true}
+            value={listType}
+            onChange={setListType}
+        />
+    );
+}
 
 
+function KeywordEntries() {
+    const update = useForceUpdater();
+    const [values, setValues] = useState(keywordEntries);
+
+    const elements = keywordEntries.map((entry, i) => {
+        const setRegex = (v: string) => {
+            let valuesCopy = [...values];
+            valuesCopy[i].regex = v;
+            setValues(valuesCopy);
+        };
+
+        const setListIds = (v: Array<string>) => {
+            let valuesCopy = [...values];
+            valuesCopy[i].listIds = v;
+            setValues(valuesCopy);
+        };
+
+        const setListType = (v: ListType) => {
+            let valuesCopy = [...values];
+            valuesCopy[i].listType = v;
+            setValues(valuesCopy);
+        };
+
+        return (
+            <>
+                <Collapsible title={`Keyword Entry ${i + 1}`}>
+                    <Flex flexDirection="row">
+                        <div style={{flexGrow: 1}}>
+                            <TextInput
+                                placeholder="example|regex"
+                                spellCheck={false}
+                                value={values[i].regex}
+                                onChange={setRegex}
+                                onBlur={() => setKeywordEntry(i, values[i].regex, values[i].listIds, values[i].listType)}
+                            />
+                        </div>
+                        <Button
+                            onClick={() => removeKeywordEntry(i, update)}
+                            look={Button.Looks.BLANK}
+                            size={Button.Sizes.ICON}
+                            className="keywordnotify-delete">
+                            <DeleteIcon/>
+                        </Button>
+                    </Flex>
+                    <Forms.FormDivider className={Margins.top8 + " " + Margins.bottom8}/>
+                    <Forms.FormTitle tag="h5">Whitelist/Blacklist</Forms.FormTitle>
+                    <Flex flexDirection="row">
+                        <div style={{flexGrow: 1}}>
+                            <ListedIds listIds={values[i].listIds} setListIds={setListIds}/>
+                        </div>
+                    </Flex>
+                    <div className={Margins.top8 + " " + Margins.bottom8}/>
+                    <Flex flexDirection="row">
+                        <Button onClick={() => {
+                            values[i].listIds.push("");
+                            update();
+                        }}>Add ID</Button>
+                        <div style={{flexGrow: 1}}>
+                            <ListTypeSelector listType={values[i].listType} setListType={setListType}/>
+                        </div>
+                    </Flex>
+                </Collapsible>
+            </>
+        );
+    });
+
+    return (
+        <>
+            {elements}
+            <div><Button onClick={() => addKeywordEntry(update)}>Add Keyword Entry</Button></div>
+        </>
+    );
 }
 
 const settings = definePluginSettings({
@@ -164,64 +247,8 @@ const settings = definePluginSettings({
     keywords: {
         type: OptionType.COMPONENT,
         description: "",
-        component: () => {
-            const update = useForceUpdater();
-            const [values, setValues] = useState(regexes);
-
-            const elements = regexes.map((a, i) => {
-                const setValue = (v: string) => {
-                    let valuesCopy = [...values];
-                    valuesCopy[i] = v;
-                    setValues(valuesCopy);
-                };
-
-                return (
-                    <>
-                        <Forms.FormTitle tag="h4">Keyword Regex {i + 1}</Forms.FormTitle>
-
-                        <Flex flexDirection="row">
-                            <div style={{ flexGrow: 1 }}>
-                                <TextInput
-                                    placeholder="example|regex"
-                                    spellCheck={false}
-                                    value={values[i]}
-                                    onChange={setValue}
-                                    onBlur={() => setRegexes(i, values[i])}
-                                />
-                            </div>
-                            <Button
-                                onClick={() => removeRegex(i, update)}
-                                look={Button.Looks.BLANK}
-                                size={Button.Sizes.ICON}
-                                className="keywordnotify-delete">
-                                <DeleteIcon/>
-                            </Button>
-                        </Flex>
-                    </>
-                );
-            });
-
-            return (
-                <>
-                    {elements}
-                    <div><Button onClick={() => addRegex(update)}>Add Regex</Button></div>
-                </>
-            );
-        }
-    },
-    listedIds: {
-        type: OptionType.COMPONENT,
-        description: "",
-        component: () => <ListedIds/>
-    },
-    listType: {
-        type: OptionType.SELECT,
-        description: "Select whether to use a blacklist or a whitelist",
-        options: [
-            { label: ListType.BlackList, value: ListType.BlackList, default: true },
-            { label: ListType.Whitelist , value: ListType.Whitelist }
-        ]
-    },
+        component: () => <KeywordEntries/>
+    }
 });
 
 export default definePlugin({
@@ -261,59 +288,56 @@ export default definePlugin({
     ],
 
     async start() {
-        regexes = await DataStore.get("KeywordNotify_rules") ?? [];
-        listIds = await DataStore.get("KeywordNotify_listIds") ?? [];
-        this.me = await UserUtils.getUser(UserStore.getCurrentUser().id);
+        keywordEntries = await DataStore.get("KeywordNotify_keywordEntries") ?? [];
+        currentUser = await UserUtils.getUser(UserStore.getCurrentUser().id);
         this.onUpdate = () => null;
-        this.keywordLog = [];
 
         (await DataStore.get("KeywordNotify_log") ?? []).map((e) => JSON.parse(e)).forEach((e) => {
             this.addToLog(e);
         });
     },
 
-    applyRegexes(m : Message) {
-        let listed = listedMessage(m);
-        let whitelistMode = settings.store.listType === ListType.Whitelist;
-        if (settings.store.listType === ListType.BlackList && listed) {
-            return;
-        }
-        if (whitelistMode && !listed) {
-            return;
-        }
-
-        if (settings.store.ignoreBots && m.author.bot) {
-            if (!whitelistMode || !listContains(m.author.id)) {
-                return;
-            }
-        }
-
+    applyKeywordEntries(m: Message) {
         let matches = false;
 
-        if (regexes.some(r => r != "" && (safeMatchesRegex(m.content, r)))) {
-            matches = true;
-        }
-        for (let embed of m.embeds as any) {
-            if (regexes.some(r => r != "" && (safeMatchesRegex(embed.description, r) || safeMatchesRegex(embed.title, r)))) {
-                matches = true;
-            } else if (embed.fields != null) {
-                for (let field of embed.fields as Array<{ name: string, value: string }>) {
-                    if (regexes.some(r => r != "" && (safeMatchesRegex(field.value, r) || safeMatchesRegex(field.name, r)))) {
-                        matches = true;
-                    }
+        keywordEntries.forEach(entry => {
+            let listed = entry.listIds.some(id => id === m.channel_id || id === m.author.id);
+            if (!listed) {
+                const channel = ChannelStore.getChannel(m.channel_id);
+                if (channel != null) {
+                    listed = entry.listIds.some(id => id === channel.guild_id);
                 }
             }
-        }
+
+            let whitelistMode = entry.listType === ListType.Whitelist;
+            if (entry.listType === ListType.BlackList && listed) {
+                return;
+            }
+            if (whitelistMode && !listed) {
+                return;
+            }
+
+            if (settings.store.ignoreBots && m.author.bot) {
+                if (!whitelistMode || !entry.listIds.includes(m.author.id)) {
+                    return;
+                }
+            }
+
+            if (entry.regex != "" && safeMatchesRegex(m.content, entry.regex)) {
+                matches = true;
+            }
+        });
 
         if (matches) {
-            m.mentions.push(this.me);
+            // @ts-ignore
+            m.mentions.push(currentUser);
 
-            if (m.author.id != this.me.id)
+            if (m.author.id != currentUser.id)
                 this.addToLog(m);
         }
     },
 
-    addToLog(m : Message) {
+    addToLog(m: Message) {
         if (m == null || this.keywordLog.some((e) => e.id == m.id))
             return;
 
@@ -338,21 +362,21 @@ export default definePlugin({
 
     tryKeywordMenu(setTab, onJump, closePopout) {
         let header = (
-            <MenuHeader tab={5} setTab={setTab} closePopout={closePopout} badgeState={{ badgeForYou: false }}/>
+            <MenuHeader tab={5} setTab={setTab} closePopout={closePopout} badgeState={{badgeForYou: false}}/>
         );
 
         let channel = ChannelStore.getChannel(SelectedChannelStore.getChannelId());
 
-        let [keywordLog, setKeywordLog] = useState(this.keywordLog);
+        let [tempLogs, setKeywordLog] = useState(keywordLog);
         this.onUpdate = () => {
-            let newLog = [...this.keywordLog];
+            let newLog = [...keywordLog];
             setKeywordLog(newLog);
 
             DataStore.set("KeywordNotify_log", newLog.map((e) => JSON.stringify(e)));
         };
 
         let onDelete = (m) => {
-            this.keywordLog = this.keywordLog.filter((e) => e.id != m.id);
+            keywordLog = keywordLog.filter((e) => e.id != m.id);
             this.onUpdate();
         };
 
@@ -368,7 +392,7 @@ export default definePlugin({
 
             msg.props.children[0].props.children.props.onClick = () => onDelete(e);
             msg.props.children[1].props.children[1].props.message.customRenderedContent = {
-                content: highlightKeywords(e.content, regexes)
+                content: highlightKeywords(e.content, keywordEntries.map((e) => e.regex))
             };
 
             return [msg];
@@ -385,7 +409,7 @@ export default definePlugin({
                     onFetch={() => null}
                     onCloseMessage={onDelete}
                     loadMore={() => null}
-                    messages={keywordLog}
+                    messages={tempLogs}
                     renderEmptyState={() => null}
                 />
             </>
@@ -394,10 +418,10 @@ export default definePlugin({
 
     modify(e) {
         if (e.type == "MESSAGE_CREATE") {
-            this.applyRegexes(e.message);
+            this.applyKeywordEntries(e.message);
         } else if (e.type == "LOAD_MESSAGES_SUCCESS") {
             for (let msg = 0; msg < e.messages.length; ++msg) {
-                this.applyRegexes(e.messages[msg]);
+                this.applyKeywordEntries(e.messages[msg]);
             }
         }
         return e;
